1633afc609943f7e7c23394828d04bf8beaabcaf
hamidreza-arabian <41690470+hamidreza-arabian@users.noreply.github.com>
2024-05-11 14:44:49 +0430

    create new page for stocks

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -28,12 +28,15 @@ const showingNavigationDropdown = ref(false);
                             </div>
 
                             <!-- Navigation Links -->
-                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                            <div class="hidden space-x-8 rtl:space-x-reverse  sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     داشبورد
                                 </NavLink>
                                 <NavLink :href="route('profile.edit')" :active="route().current('profile.edit')">
                                     پروفایل
+                                </NavLink>
+                                <NavLink :href="route('stock.index')" :active="route().current('stock.index')">
+                                    سهام های تحت نظر
                                 </NavLink>
                             </div>
                         </div>
